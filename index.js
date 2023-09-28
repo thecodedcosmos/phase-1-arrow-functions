d2b7ec380a0122c4360b33f37f73e7d263669959
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-09-27 23:48:30 -0700

    Complete Lab

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function() {
+  return 2000 / 100;
+}
+const square = (parameter1) => {
+    return parameter1 * parameter1;
+}
+const add = (parameter1, parameter2) => {
+    return parameter1 + parameter2;
+}
